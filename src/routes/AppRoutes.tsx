d7ce861efa3767f858b3f53d0a347f215fb43bd8
Fayozbek777 <fayozbekbaxromov9@gmail.com>
2026-07-29 13:16:57 +0500

    feat: add tasks page with static data

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Login from "../components/features/auth/Login";
 import Dashboard from "../components/features/dashboard/Dashboard";
 import Layout from "../components/common/Layout/Layout";
 import EmployeeList from "../components/features/Employees/EmployeesList";
+import TaskList from "../components/features/Tasks/TasksList";
 
 const AppRoutes = () => {
   return (
@@ -14,7 +15,7 @@ const AppRoutes = () => {
         <Route index element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/employees" element={<EmployeeList />} />
-        <Route path="/tasks" element={<div className="p-8">Tasks Page</div>} />
+        <Route path="/tasks" element={<TaskList />} />
         <Route
           path="/profile"
           element={<div className="p-8">Profile Page</div>}
