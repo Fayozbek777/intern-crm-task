dbe3cb3f4424ef5a3b9dbe0be6df665d93217e5c
Fayozbek777 <fayozbekbaxromov9@gmail.com>
2026-07-29 13:13:36 +0500

    feat: add employees page with static data

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../components/features/auth/Login";
 import Dashboard from "../components/features/dashboard/Dashboard";
 import Layout from "../components/common/Layout/Layout";
+import EmployeeList from "../components/features/Employees/EmployeesList";
 
 const AppRoutes = () => {
   return (
@@ -12,10 +13,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route
-          path="/employees"
-          element={<div className="p-8">Employees Page</div>}
-        />
+        <Route path="/employees" element={<EmployeeList />} />
         <Route path="/tasks" element={<div className="p-8">Tasks Page</div>} />
         <Route
           path="/profile"
